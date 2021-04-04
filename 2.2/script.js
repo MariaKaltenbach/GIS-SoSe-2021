f9ffad546c2e9e0e57d97621373427bf89992819
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2021-04-04 09:35:56 +0200

    Scripting Aufgaben fertig gemacht

--- 2.2/script.js
+++ 2.2/script.js
@@ -193,31 +193,4 @@ context.fillStyle = "green";
 context.fill();
 context.strokeStyle = "lightgreen";
 context.stroke();
-// Aufgabe 1 woche 3
-/*
-let buttons: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName("button");
-buttons[0].addEventListener("click", makeNew);
-buttons[1].addEventListener("click", reset);
-function makeNew(_e: Event): void {
-    let div: HTMLDivElement = document.createElement("div");
-    div.style.backgroundColor = "black";
-    div.style.height = "50px";
-    div.style.width = "50px";
-    div.style.marginLeft = (Math.random() * 200).toString() + "px";
-    document.getElementById("fillThis")?.appendChild(div);
-}
-function reset(_e: Event): void {
-let parent: HTMLDivElement = <HTMLDivElement>document.getElementById("fillThis");
-parent.innerHTML = "";
-}
-*/
-/*     <button>
-    new
-</button>
-    <button>
-    refresh
-</button>
-    <div id="fillThis">
-    </div>
-*/
 //# sourceMappingURL=script.js.map
