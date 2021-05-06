bf7d5024f6c87f5293c5acba87b4ef0b7a258ff0
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2021-05-06 12:39:37 +0200

    Canvas an responsive design angepasst

--- 2.3/script.js
+++ 2.3/script.js
@@ -21,96 +21,100 @@ if (page == "aufgabe1.html") {
         parent.innerHTML = "";
     }
 }
-let waffelVariation;
-let eiskugelVariation;
-let streuselVariation;
-//mit Canvas zeichnen
-let canvas = document.getElementById("myEiscreme");
-let context = canvas.getContext("2d");
-context.lineWidth = 3;
-if (page == "Waffel.html") {
-    context.beginPath();
-    context.moveTo(50, 200);
-    context.lineTo(150, 600);
-    context.lineTo(250, 200);
-    context.fill();
-    context.closePath();
-    context.stroke();
-}
-if (page == "Streusel.html") {
-    context.beginPath();
-    context.moveTo(80, 140);
-    context.lineTo(100, 150);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(170, 140);
-    context.lineTo(150, 150);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(230, 100);
-    context.lineTo(210, 110);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(180, 60);
-    context.lineTo(190, 80);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(130, 100);
-    context.lineTo(100, 100);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(100, 60);
-    context.lineTo(130, 40);
-    context.closePath();
-    context.stroke();
-    context.beginPath();
-    context.moveTo(190, 160);
-    context.lineTo(210, 180);
-    context.closePath();
-    context.stroke();
-}
-if (page == "Eiskugel.html") {
-    context.beginPath();
-    context.arc(150, 130, 120, 0, 2 * Math.PI);
-    context.closePath();
-    context.fill();
-    context.stroke();
-}
-//Auswahlmöglichkeiten aus data.js laden 
-let waffel = document.getElementById("waffel");
-let eiskugel = document.getElementById("eiskugel");
-let streusel = document.getElementById("streusel");
-//for-Schleife um die Daten ausgeben zu lassen
-for (let i = 0; i < eiskugelVariation.length; i++) {
-    let newOptionElement = document.createElement("OPTION");
-    newOptionElement.innerText = eiskugelVariation[i].name;
-    eiskugel.appendChild(newOptionElement);
-}
-for (let i = 0; i < waffelVariation.length; i++) {
-    let newOptionElement = document.createElement("OPTION");
-    newOptionElement.innerText = waffelVariation[i].name;
-    waffel.appendChild(newOptionElement);
-}
-for (let i = 0; i < streuselVariation.length; i++) {
-    let newOptionElement = document.createElement("OPTION");
-    newOptionElement.innerText = streuselVariation[i].name;
-    streusel.appendChild(newOptionElement);
-}
-waffel.addEventListener("change", waffelVariationChanged);
-function waffelVariationChanged(_e) {
-    console.log(_e.target.value);
-}
-eiskugel.addEventListener("change", eiskugelVariationChanged);
-function eiskugelVariationChanged(_e) {
-    console.log(_e.target.value);
-}
-streusel.addEventListener("change", streuselVariationChanged);
-function streuselVariationChanged(_e) {
-    console.log(_e.target.value);
-}
+// Aufagabe 2 und 3 
+var Eisladen;
+(function (Eisladen) {
+    let waffelVariation;
+    let eiskugelVariation;
+    let streuselVariation;
+    //mit Canvas zeichnen
+    let canvas = document.getElementById("myEiscreme");
+    let context = canvas.getContext("2d");
+    context.lineWidth = 3;
+    if (page == "Waffel.html") {
+        context.beginPath();
+        context.moveTo(50, 200);
+        context.lineTo(150, 600);
+        context.lineTo(250, 200);
+        context.fill();
+        context.closePath();
+        context.stroke();
+    }
+    if (page == "Streusel.html") {
+        context.beginPath();
+        context.moveTo(80, 140);
+        context.lineTo(100, 150);
+        context.closePath();
+        context.stroke();
+        context.beginPath();
+        context.moveTo(170, 140);
+        context.lineTo(150, 150);
+        context.closePath();
+        context.stroke();
+        context.beginPath();
+        context.moveTo(230, 100);
+        context.lineTo(210, 110);
+        context.closePath();
+        context.stroke();
+        context.beginPath();
+        context.moveTo(180, 60);
+        context.lineTo(190, 80);
+        context.closePath();
+        context.stroke();
+        context.beginPath();
+        context.moveTo(130, 100);
+        context.lineTo(100, 100);
+        context.closePath();
+        context.stroke();
+        context.beginPath();
+        context.moveTo(100, 60);
+        context.lineTo(130, 40);
+        context.closePath();
+        context.stroke();
+        context.beginPath();
+        context.moveTo(190, 160);
+        context.lineTo(210, 180);
+        context.closePath();
+        context.stroke();
+    }
+    if (page == "Eiskugel.html") {
+        context.beginPath();
+        context.arc(150, 130, 120, 0, 2 * Math.PI);
+        context.closePath();
+        context.fill();
+        context.stroke();
+    }
+    //Auswahlmöglichkeiten aus data.js laden 
+    let eiskugelSelect = document.getElementById("eiskugel");
+    let waffelSelect = document.getElementById("waffel");
+    let streuselSelect = document.getElementById("streusel");
+    //for-Schleife um die Daten ausgeben zu lassen
+    for (let i = 0; i < eiskugelVariation.length; i++) {
+        let newOptionElement = document.createElement("OPTION");
+        newOptionElement.innerText = eiskugelVariation[i].name;
+        eiskugelSelect.appendChild(newOptionElement);
+    }
+    for (let i = 0; i < waffelVariation.length; i++) {
+        let newOptionElement = document.createElement("OPTION");
+        newOptionElement.innerText = waffelVariation[i].name;
+        waffelSelect.appendChild(newOptionElement);
+    }
+    for (let i = 0; i < streuselVariation.length; i++) {
+        let newOptionElement = document.createElement("OPTION");
+        newOptionElement.innerText = streuselVariation[i].name;
+        streuselSelect.appendChild(newOptionElement);
+    }
+    waffelSelect.addEventListener("change", waffelVariationChanged);
+    eiskugelSelect.addEventListener("change", eiskugelVariationChanged);
+    streuselSelect.addEventListener("change", streuselVariationChanged);
+    function waffelVariationChanged(_e) {
+        console.log(_e.target.value);
+    }
+    function eiskugelVariationChanged(_e) {
+        console.log(_e.target.value);
+    }
+    function streuselVariationChanged(_e) {
+        console.log(_e.target.value);
+    }
+})(Eisladen || (Eisladen = {}));
 //# sourceMappingURL=script.js.map
--- 2.3/script.ts
+++ 2.3/script.ts
@@ -1,4 +1,3 @@
-
 //path variable angelegt -> damit ich mit einer if abfrage prüfen kann auf welcher seite ich bin, damit 
 //man die komponeneten einzeln auf verschiedenen seiten auswählen kann
 let path: string = window.location.pathname;
@@ -22,155 +21,165 @@ if (page == "aufgabe1.html") {
         let parent: HTMLDivElement = <HTMLDivElement>document.getElementById("fillThis");
         parent.innerHTML = "";
     }
+
+
+
 }
 
 // Aufagabe 2 und 3 
 
-//region Interface (Interface für alle drei Auswahlmöglichkeiten angelegt)
-interface Lebensmittel {
+namespace Eisladen {
 
-    geschmack: string;
-    preis: number;
-    name: string;
-    farbe: string;
-}
+    //region Interface (Interface für alle drei Auswahlmöglichkeiten angelegt)
+    export interface Lebensmittel {
 
+        geschmack: string;
+        preis: number;
+        name: string;
+        farbe: string;
+    }
 
-//(Listen für alles angelegt)
-interface LebensmittelListe {
+    let waffelVariation: Lebensmittel[];
+    let eiskugelVariation: Lebensmittel[];
+    let streuselVariation: Lebensmittel[];
 
-    waffelListe: Lebensmittel[];
-    eiskugelListe: Lebensmittel[];
-    streuselListe: Lebensmittel[];
-}
+  
 
-let waffelVariation: Lebensmittel[];
-let eiskugelVariation: Lebensmittel[];
-let streuselVariation: Lebensmittel[];
+    //mit Canvas zeichnen
 
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("myEiscreme");
+    let context: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
 
-//mit Canvas zeichnen
+    context.lineWidth = 3;
 
-let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("myEiscreme");
-let context: CanvasRenderingContext2D = <CanvasRenderingContext2D>canvas.getContext("2d");
+    if (page == "Waffel.html") {
 
-context.lineWidth = 3;
-
-if (page == "Waffel.html") {
-
-    context.beginPath();
-    context.moveTo(50, 200);
-    context.lineTo(150, 600);
-    context.lineTo(250, 200);
-    context.fill();
-    context.closePath();
-    context.stroke();
+        context.beginPath();
+        context.moveTo(50, 200);
+        context.lineTo(150, 600);
+        context.lineTo(250, 200);
+        context.fill();
+        context.closePath();
+        context.stroke();
 
 
-}
+    }
 
-if (page == "Streusel.html") {
+    if (page == "Streusel.html") {
 
-    context.beginPath();
-    context.moveTo(80, 140);
-    context.lineTo(100, 150);
-    context.closePath();
-    context.stroke();
+        context.beginPath();
+        context.moveTo(80, 140);
+        context.lineTo(100, 150);
+        context.closePath();
+        context.stroke();
 
-    context.beginPath();
-    context.moveTo(170, 140);
-    context.lineTo(150, 150);
-    context.closePath();
-    context.stroke();
+        context.beginPath();
+        context.moveTo(170, 140);
+        context.lineTo(150, 150);
+        context.closePath();
+        context.stroke();
 
-    context.beginPath();
-    context.moveTo(230, 100);
-    context.lineTo(210, 110);
-    context.closePath();
-    context.stroke();
+        context.beginPath();
+        context.moveTo(230, 100);
+        context.lineTo(210, 110);
+        context.closePath();
+        context.stroke();
 
-    context.beginPath();
-    context.moveTo(180, 60);
-    context.lineTo(190, 80);
-    context.closePath();
-    context.stroke();
+        context.beginPath();
+        context.moveTo(180, 60);
+        context.lineTo(190, 80);
+        context.closePath();
+        context.stroke();
 
-    context.beginPath();
-    context.moveTo(130, 100);
-    context.lineTo(100, 100);
-    context.closePath();
-    context.stroke();
+        context.beginPath();
+        context.moveTo(130, 100);
+        context.lineTo(100, 100);
+        context.closePath();
+        context.stroke();
 
-    context.beginPath();
-    context.moveTo(100, 60);
-    context.lineTo(130, 40);
-    context.closePath();
-    context.stroke();
+        context.beginPath();
+        context.moveTo(100, 60);
+        context.lineTo(130, 40);
+        context.closePath();
+        context.stroke();
 
-    context.beginPath();
-    context.moveTo(190, 160);
-    context.lineTo(210, 180);
-    context.closePath();
-    context.stroke();
-}
+        context.beginPath();
+        context.moveTo(190, 160);
+        context.lineTo(210, 180);
+        context.closePath();
+        context.stroke();
+    }
 
 
-if (page == "Eiskugel.html") {
+    if (page == "Eiskugel.html") {
 
-    context.beginPath();
-    context.arc(150, 130, 120, 0, 2 * Math.PI);
-    context.closePath();
-    context.fill();
-    context.stroke();
-}
+        context.beginPath();
+        context.arc(150, 130, 120, 0, 2 * Math.PI);
+        context.closePath();
+        context.fill();
+        context.stroke();
+    }
 
-//Auswahlmöglichkeiten aus data.js laden 
 
-let waffel: HTMLSelectElement = <HTMLSelectElement>document.getElementById("waffel");
-let eiskugel: HTMLSelectElement = <HTMLSelectElement>document.getElementById("eiskugel");
-let streusel: HTMLSelectElement = <HTMLSelectElement>document.getElementById("streusel");
+    //Auswahlmöglichkeiten aus data.js laden 
 
-//for-Schleife um die Daten ausgeben zu lassen
+    let eiskugelSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById("eiskugel");
+    let waffelSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById("waffel");
+    let streuselSelect: HTMLSelectElement = <HTMLSelectElement>document.getElementById("streusel");
 
-for (let i: number = 0; i < eiskugelVariation.length; i++) {
-    let newOptionElement: HTMLOptionElement = <HTMLOptionElement>document.createElement("OPTION");
-    newOptionElement.innerText = eiskugelVariation[i].name;
-    eiskugel.appendChild(newOptionElement);
-}
+    //for-Schleife um die Daten ausgeben zu lassen
+   
+
+    for (let i: number = 0; i < eiskugelVariation.length; i++) {
+        let newOptionElement: HTMLOptionElement = <HTMLOptionElement>document.createElement("OPTION");
+        newOptionElement.innerText = eiskugelVariation[i].name;
+        eiskugelSelect.appendChild(newOptionElement);
+
+    }
 
 
 
-for (let i: number = 0; i < waffelVariation.length; i++) {
-    let newOptionElement: HTMLOptionElement = <HTMLOptionElement>document.createElement("OPTION");
-    newOptionElement.innerText = waffelVariation[i].name;
-    waffel.appendChild(newOptionElement);
-}
+    for (let i: number = 0; i < waffelVariation.length; i++) {
+        let newOptionElement: HTMLOptionElement = <HTMLOptionElement>document.createElement("OPTION");
+        newOptionElement.innerText = waffelVariation[i].name;
+        waffelSelect.appendChild(newOptionElement);
+        
+    }
 
 
 
-for (let i: number = 0; i < streuselVariation.length; i++) {
-    let newOptionElement: HTMLOptionElement = <HTMLOptionElement>document.createElement("OPTION");
-    newOptionElement.innerText = streuselVariation[i].name;
-    streusel.appendChild(newOptionElement);
-}
+    for (let i: number = 0; i < streuselVariation.length; i++) {
+        let newOptionElement: HTMLOptionElement = <HTMLOptionElement>document.createElement("OPTION");
+        newOptionElement.innerText = streuselVariation[i].name;
+        streuselSelect.appendChild(newOptionElement);
+    }
 
 
-waffel.addEventListener("change", waffelVariationChanged);
 
-function waffelVariationChanged(_e: Event): void {
-    console.log((<HTMLOptionElement>_e.target).value);
-}
-
-
-eiskugel.addEventListener("change", eiskugelVariationChanged);
-
-function eiskugelVariationChanged(_e: Event): void {
-    console.log((<HTMLOptionElement>_e.target).value);
-}
+    waffelSelect.addEventListener("change", waffelVariationChanged);
+    eiskugelSelect.addEventListener("change", eiskugelVariationChanged);
+    streuselSelect.addEventListener("change", streuselVariationChanged);
 
 
-streusel.addEventListener("change", streuselVariationChanged);
+    function waffelVariationChanged(_e: Event): void {
+        console.log((<HTMLOptionElement>_e.target).value);
 
-function streuselVariationChanged(_e: Event): void {
-    console.log((<HTMLOptionElement>_e.target).value);
+    }
+
+
+    function eiskugelVariationChanged(_e: Event): void {
+        console.log((<HTMLOptionElement>_e.target).value);
+
+    }
+
+
+    function streuselVariationChanged(_e: Event): void {
+        console.log((<HTMLOptionElement>_e.target).value);
+
+    }
+
+
+
+      
+
 }
