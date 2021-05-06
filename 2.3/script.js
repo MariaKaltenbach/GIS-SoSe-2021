65067edd3656a0e2fa198fd65e4487c05563afc9
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2021-05-06 16:04:53 +0200

    Aufgabe 1 in einen anderen Ordner verschoben

--- 2.3/script.js
+++ 2.3/script.js
@@ -3,24 +3,6 @@
 //man die komponeneten einzeln auf verschiedenen seiten auswählen kann
 let path = window.location.pathname;
 let page = path.split("/").pop();
-// Aufgabe 1 woche 3
-if (page == "aufgabe1.html") {
-    let buttons = document.getElementsByTagName("button");
-    buttons[0].addEventListener("click", makeNew);
-    buttons[1].addEventListener("click", reset);
-    function makeNew(_e) {
-        let div = document.createElement("div");
-        div.style.backgroundColor = "black";
-        div.style.height = "50px";
-        div.style.width = "50px";
-        div.style.marginLeft = (Math.random() * 200).toString() + "px";
-        document.getElementById("fillThis")?.appendChild(div);
-    }
-    function reset(_e) {
-        let parent = document.getElementById("fillThis");
-        parent.innerHTML = "";
-    }
-}
 // Aufagabe 2 und 3 
 var Eisladen;
 (function (Eisladen) {
--- 2.3/script.ts
+++ 2.3/script.ts
@@ -4,27 +4,6 @@ let path: string = window.location.pathname;
 let page: string = path.split("/").pop();
 
 
-// Aufgabe 1 woche 3
-if (page == "aufgabe1.html") {
-    let buttons: HTMLCollectionOf<HTMLButtonElement> = document.getElementsByTagName("button");
-    buttons[0].addEventListener("click", makeNew);
-    buttons[1].addEventListener("click", reset);
-    function makeNew(_e: Event): void {
-        let div: HTMLDivElement = document.createElement("div");
-        div.style.backgroundColor = "black";
-        div.style.height = "50px";
-        div.style.width = "50px";
-        div.style.marginLeft = (Math.random() * 200).toString() + "px";
-        document.getElementById("fillThis")?.appendChild(div);
-    }
-    function reset(_e: Event): void {
-        let parent: HTMLDivElement = <HTMLDivElement>document.getElementById("fillThis");
-        parent.innerHTML = "";
-    }
-
-
-}
-
 // Aufagabe 2 und 3 
 
 namespace Eisladen {
