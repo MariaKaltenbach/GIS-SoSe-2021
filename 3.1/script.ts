7f0cbc03bd0de2d55c18e6785820c8781a475f7e
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2021-05-28 14:21:33 +0200

    3.1 fertig

--- 3.1/script.ts
+++ 3.1/script.ts
@@ -1,63 +1,21 @@
-import * as Http from "http";
 
 export namespace P_3_1Server {
 
-    //nlet url: string = "https://gissose2021.herokuapp.com/";
-    let url: string = "http://localhost:8100";
-    let form: HTMLFormElement;
+ let url: string = "https://gissose2021.herokuapp.com/";
+ //let url: string = "http://localhost:8100";
+ let form: HTMLFormElement;
 
-    //"Startng Server" wird auf der console ausgegeben
-    console.log("Starting server");
+ let senden: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button");
+ senden.addEventListener("click", sendData);
 
-    //enviroment mit der Angabe der Portnummer von Heroku
-    let port: number = Number(process.env.PORT);
+ async function sendData(_event: Event): Promise<void> {
+     let formData: FormData = new FormData(form);
+     let query: URLSearchParams = new URLSearchParams(<undefined>formData);
+     let response: Response = await fetch(url + "?" + query.toString());
+     let responseText: String = await response.text();
+     await fetch("index.html?" + MediaQueryList.toString());
+     alert(responseText);
+     console.log(query);
 
-    //wenn port nicht definiert ist (keiner von heroku zugewiesen wurde), wird port nr. 8100 aufgerufen
-    if (!port)
-        port = 8100;
-
-    //server wird erstellt    
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    //Server horcht auf port
-    server.listen(port);
-
-    //es wird auf der cosnole ausgegeben wenn der server funktioniert 
-    function handleListen(): void {
-        console.log("Listening");
-    }
-
-
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
-        //Header wird festgelegt
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        //sicherheitsmechanisemn werden abgeschaltet
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        //die Url der anfrage mit der antwort wird angezeigt
-        _response.write(_request.url);
-        //und aud der console ausgegeben
-        console.log(_request.url);
-        //antwort wird beendet
-        _response.end();
-    }
-    let senden: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button");
-    senden.addEventListener("click", sendData);
-
-    async function sendData(_event: Event): Promise<void> {
-        let formData: FormData = new FormData(form);
-        let query: URLSearchParams = new URLSearchParams(<undefined>formData);
-        let response: Response = await fetch(url + "?" + query.toString());
-        let responseText: String = await response.text();
-        await fetch("index.html?" + MediaQueryList.toString());
-        alert(responseText);
-        console.log(query);
-
-    }
-
-
-
-}
-
-
+ }
+ }
---
+++ 3.1/server.ts
@@ -0,0 +1,39 @@
+import * as Http from "http";
+
+export namespace P_3_1Server {
+//"Startng Server" wird auf der console ausgegeben
+console.log("Starting server");
+
+//enviroment mit der Angabe der Portnummer von Heroku
+let port: number = Number(process.env.PORT);
+
+//wenn port nicht definiert ist (keiner von heroku zugewiesen wurde), wird port nr. 8100 aufgerufen
+if (!port)
+    port = 8100;
+
+//server wird erstellt    
+let server: Http.Server = Http.createServer();
+server.addListener("request", handleRequest);
+server.addListener("listening", handleListen);
+//Server horcht auf port
+server.listen(port);
+
+//es wird auf der cosnole ausgegeben wenn der server funktioniert 
+function handleListen(): void {
+    console.log("Listening");
+}
+
+
+function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    console.log("I hear voices!");
+    //Header wird festgelegt
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    //sicherheitsmechanisemn werden abgeschaltet
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    //die Url der anfrage mit der antwort wird angezeigt
+    _response.write(_request.url);
+    //und aud der console ausgegeben
+    console.log(_request.url);
+    //antwort wird beendet
+    _response.end();
+} }
