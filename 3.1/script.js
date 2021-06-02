d9508fae6ad1486389bcd001a986a5afe8f829cc
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2021-06-02 16:10:32 +0200

    server ausgabe

--- 3.1/script.js
+++ 3.1/script.js
@@ -5,17 +5,17 @@ let form;
 var P_3_1Server;
 (function (P_3_1Server) {
     let url = "https://gissose2021.herokuapp.com/";
+    let senden = document.querySelector("button");
+    senden.addEventListener("click", sendData);
     async function sendData(_event) {
         let formData = new FormData(form);
         let query = new URLSearchParams(formData);
         let response = await fetch(url + "?" + query.toString());
         let responseText = await response.text();
         await fetch("index.html?" + MediaQueryList.toString());
-        console.log(query);
+        console.log(responseText);
         let rueckgabe = document.getElementById("serverausgabe");
         rueckgabe.innerText = responseText;
     }
-    let senden = document.querySelector("button");
-    senden.addEventListener("click", sendData);
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
 //# sourceMappingURL=script.js.map
--- 3.1/script.ts
+++ 3.1/script.ts
@@ -2,22 +2,22 @@ let form: HTMLFormElement;
 export namespace P_3_1Server {
 
     let url: string = "https://gissose2021.herokuapp.com/";
-   
-
-    async function sendData(_event: Event): Promise<void> {
-        let formData: FormData = new FormData(form);
-        let query: URLSearchParams = new URLSearchParams(<undefined>formData);
-        let response: Response = await fetch(url + "?" + query.toString());
-        let responseText: string = await response.text();
-        await fetch("index.html?" + MediaQueryList.toString());
-        console.log(query);
-
-        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("serverausgabe");
-        rueckgabe.innerText = responseText;
-
-    }
-
 
     let senden: HTMLButtonElement = <HTMLButtonElement>document.querySelector("button");
     senden.addEventListener("click", sendData);
+
+    async function sendData(_event: Event): Promise<void> {
+        let formData: FormData = new FormData(form);
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        let response: Response = await fetch(url + "?" + query.toString());
+        let responseText: string = await response.text();
+        await fetch("index.html?" + MediaQueryList.toString());
+        console.log(responseText);
+        
+    
+        let rueckgabe: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverausgabe"); 
+        rueckgabe.innerText = responseText; 
+    
+    }
+
 }
