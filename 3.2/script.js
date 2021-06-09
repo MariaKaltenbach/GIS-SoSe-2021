285140521b5a49a4bfd051f045fb21489875a6de
MariaKaltenbach <72461966+MariaKaltenbach@users.noreply.github.com>
2021-06-09 18:12:32 +0200

    3.2 überarbeitet

--- 3.2/script.js
+++ 3.2/script.js
@@ -1,33 +1,32 @@
 "use strict";
 var Aufgabe3_2;
 (function (Aufgabe3_2) {
-    //mit ID server Antwort in html speichern 
     let serverResponse = document.getElementById("serverAntwort");
     let htmlButton = document.getElementById("htmlButton");
-    htmlButton.addEventListener("click", htmlLaden); //eventlistener wir aktiviert und htmlLaden funktion wird aufgerufen wenn man den htmlButton clickt 
+    htmlButton.addEventListener("click", htmlLaden); //eventlistener wir aktiviert und htmlLaden funktion wird aufgerufen wenn man den htmlButton clickt
     async function htmlLaden() {
-        let formData = new FormData(document.forms[0]);
-        let url = "https://gissose2021.herokuapp.com/"; //herokuapp link einfügen als url variable 
+        let daten = new FormData(document.forms[0]);
+        //let url: RequestInfo = "hhttps://gissose2021.herokuapp.com"; //herokuapp link einfügen als url variable 
+        let url = "http://localhost:8100";
         url += "/html";
-        let query = new URLSearchParams(formData);
-        url = url += "?" + query.toString(); //herokuapp url mit entsandenem url zusammenfügen
+        let query = new URLSearchParams(daten);
+        url = url + "?" + query.toString(); //herokuapp url mit entsandenem url zusammenfügen
         let response = await fetch(url); //auf url warten
         let responseText = await response.text();
         serverResponse.innerHTML = responseText; //server antwort auf der Html seite aufgeben lassen 
     }
     let jsonButton = document.getElementById("jsonButton");
-    jsonButton.addEventListener("click", jsonLaden); //eventlistener wir aktiviert und jsonLaden funktion wird aufgerufen wenn man den jsonButton clickt 
+    jsonButton.addEventListener("click", jsonLaden);
     async function jsonLaden() {
-        let formData = new FormData(document.forms[0]); //Form data objekt erzeugen
-        let url = "https://gissose2021.herokuapp.com/";
+        let daten = new FormData(document.forms[0]);
+        // let url: RequestInfo = "https://gissose2021.herokuapp.com"; //herokuapp link einfügen als url variable 
+        let url = "http://localhost:8100";
         url += "/json";
-        let query = new URLSearchParams(formData); //query variable um mit Query String arebitren zu können 
-        url = url += "?" + query.toString();
-        let response = await fetch(url);
-        let responseText = await response.text();
-        console.log(responseText);
-        let responseJSON = JSON.parse(responseText); //string in Json umwandeln
-        console.log(responseJSON); //umgewndelter string ausgeben lassen
+        let query = new URLSearchParams(daten);
+        url = url + "?" + query.toString(); //herokuapp url mit entsandenem url zusammenfügen
+        let response = await fetch(url); //auf url warten
+        let jsonObjekt = await response.json(); //json okject erstellen
+        console.log(jsonObjekt); //json string in console ausgeben lassen 
     }
 })(Aufgabe3_2 || (Aufgabe3_2 = {}));
 //# sourceMappingURL=script.js.map
--- 3.2/script.ts
+++ 3.2/script.ts
@@ -1,42 +1,45 @@
 namespace Aufgabe3_2 {
 
-   //mit ID server Antwort in html speichern 
-    let serverResponse: HTMLElement = <HTMLElement>document.getElementById("serverAntwort");  
+    interface Datenobjekt { 
+        vorname: string;
+        nachname: string;
+        alter: number;
+        email: string;
+    } // interface für json object
+    let serverResponse: HTMLParagraphElement = <HTMLParagraphElement> document.getElementById("serverAntwort"); 
 
-
-
-    let htmlButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("htmlButton");
-    htmlButton.addEventListener("click", htmlLaden); //eventlistener wir aktiviert und htmlLaden funktion wird aufgerufen wenn man den htmlButton clickt 
-
+    let htmlButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("htmlButton");
+    htmlButton.addEventListener("click", htmlLaden); //eventlistener wir aktiviert und htmlLaden funktion wird aufgerufen wenn man den htmlButton clickt
 
     async function htmlLaden(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);
-        let url: string = "https://gissose2021.herokuapp.com/";   //herokuapp link einfügen als url variable 
-        url += "/html";
-        let query: URLSearchParams = new URLSearchParams(<undefined>formData);
-        url = url += "?" + query.toString();  //herokuapp url mit entsandenem url zusammenfügen
+        let daten: FormData = new FormData(document.forms[0]); 
+        //let url: RequestInfo = "hhttps://gissose2021.herokuapp.com"; //herokuapp link einfügen als url variable 
+        let url: RequestInfo = "http://localhost:8100"; 
+        url += "/html"; 
+        let query: URLSearchParams = new URLSearchParams(<undefined>daten);
+        url = url + "?" + query.toString();  //herokuapp url mit entsandenem url zusammenfügen
         let response: Response = await fetch(url);  //auf url warten
-        let responseText: string = await response.text();       
-        serverResponse.innerHTML = responseText;   //server antwort auf der Html seite aufgeben lassen 
+        let responseText: string = await response.text(); 
+        serverResponse.innerHTML = responseText; //server antwort auf der Html seite aufgeben lassen 
+          
     }
-
-
-    let jsonButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("jsonButton");
-    jsonButton.addEventListener("click", jsonLaden); //eventlistener wir aktiviert und jsonLaden funktion wird aufgerufen wenn man den jsonButton clickt 
-
+    let jsonButton: HTMLButtonElement = <HTMLButtonElement> document.getElementById("jsonButton");
+    jsonButton.addEventListener("click", jsonLaden);
+   
 
     async function jsonLaden(): Promise<void> {
-        let formData: FormData = new FormData(document.forms[0]);  //Form data objekt erzeugen
-        let url: string = "https://gissose2021.herokuapp.com/";
-        url += "/json";
-        let query: URLSearchParams = new URLSearchParams(<undefined>formData); //query variable um mit Query String arebitren zu können 
-        url = url += "?" + query.toString();
-        let response: Response = await fetch(url);
-        let responseText: string = await response.text();
-        console.log(responseText);
-        let responseJSON: JSON = JSON.parse(responseText); //string in Json umwandeln
-        console.log(responseJSON);  //umgewndelter string ausgeben lassen
-
-        
+        let daten: FormData = new FormData(document.forms[0]);
+       // let url: RequestInfo = "https://gissose2021.herokuapp.com"; //herokuapp link einfügen als url variable 
+        let url: RequestInfo = "http://localhost:8100"; 
+        url += "/json"; 
+        let query: URLSearchParams = new URLSearchParams(<undefined>daten);
+        url = url + "?" + query.toString(); //herokuapp url mit entsandenem url zusammenfügen
+        let response: Response = await fetch(url);  //auf url warten
+        let jsonObjekt: Datenobjekt = await response.json(); //json okject erstellen
+        console.log(jsonObjekt); //json string in console ausgeben lassen 
     }
+
+
+
+    
 }
